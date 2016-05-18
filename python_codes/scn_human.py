98aa34b363cbae8531796052cf4b1710b75113f2
axelcournac <acournac@pasteur.fr>
2016-05-18 18:29:07 +0200

    Add files via upload

---
+++ python_codes/scn_human.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *  
+    
+# Normalisation function
+# A is the matrix to normalise  
+# the treshold to remove bins from the normalization but not from the matrice  
+     
+def scn_func(A,threshold):    
+    n1 = A.shape[0];
+    n_iterations=10;
+    keep = np.zeros((n1, 1));
+    
+    for i in range(0,n1) :
+        if np.sum(A[i,]) > threshold:
+            keep[i] = 1
+        else :
+            keep[i] = 0
+    
+    indices1=np.where(keep >0 );
+    indices2=np.where(keep <=0 );
+    
+    for n in range(0,n_iterations) :
+        print n;
+        for i in range(0,n1) :
+            A[indices1,i]=A[indices1,i]/ np.sum(A[indices1,i]);
+            A[indices2,i]=0;    
+        A[np.isnan(A)] = 0.0;  
+        
+        for i in range(0,n1) :    
+            A[i,indices1]=A[i,indices1]/ np.sum(A[i,indices1]);
+            A[i,indices2]=0;   
+        A[np.isnan(A)] = 0.0;     
+        
+    return A
+    
