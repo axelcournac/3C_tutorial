a9c273c7313ff727c5b7d119f72a5f3dd3df2570
axelcournac <acournac@pasteur.fr>
2016-06-27 21:53:50 +0200

    Update fragment_attribution.py

--- python_codes/fragment_attribution.py
+++ python_codes/fragment_attribution.py
@@ -13,8 +13,8 @@ from Bio import Restriction
 from Bio.Seq import Seq
 from Bio.Restriction import *
 
-print "Arguments to enter:";
-print "Path for fasta files of genome (ex: /run/media/axel/human_genome/), Restriction enzyme (ex: HindIII), Alignment file (ex: /data/output_alignment_idpt.dat)";
+print("Arguments to enter:")
+print("Path for fasta files of genome (ex: /run/media/axel/human_genome/), Restriction enzyme (ex: HindIII), Alignment file (ex: /data/output_alignment_idpt.dat)")
 
 #path="/run/media/axel/RSG3/human_genome/";   #  path containing the fasta files of the chromosomes 
 path=sys.argv[1]+"/";
@@ -24,14 +24,14 @@ list_files2 =  glob.glob(path+"*.fasta");
 list_files3 =  glob.glob(path+"*.fas");
 list_files4 =  glob.glob(path+"*.fna");
 list_files=list_files1+list_files2+list_files3;
-print "List of fasta files found:";
-print list_files; 
+print("List of fasta files found:";
+print(list_files; 
 
 #enz=HindIII;   #   Restriction enzyme used in the experiment 
 enz=sys.argv[2];
-print "Enzyme used:"+str(enz);
-print "Output file used:";
-print sys.argv[3];
+print("Enzyme used:"+str(enz))
+print("Output file used:")
+print(sys.argv[3];
 
 rb = RestrictionBatch([enz]);    #  Restriction batch containing the restriction enzyme
 restriction_table={};
@@ -39,9 +39,9 @@ restriction_table={};
 for chr_file in list_files :
     handle = open(chr_file, "rU")
     for rec in SeqIO.parse(handle, "fasta"):
-        print "Cutting the following chromosome:"
+        print("Cutting the following chromosome:")
         chr_name=rec.id;
-        print chr_name;   
+        print(chr_name)
         # Building of the restriction map:
         #map_restriction = enz.search(rec.seq);
         map_restriction = rb.search(rec.seq);
@@ -56,12 +56,12 @@ for chr_file in list_files :
         # Adding end position of the chromosome to the end positions of restriction fragments:
         end_positions = np.insert(end_positions, len(end_positions), len(rec));
         restriction_table[chr_name] = vstack((start_positions,end_positions));
-        print "Number of restriction sites for "+chr_name+" for the enzyme "+str(enzyme);
-        print len(map_restriction);
+        print("Number of restriction sites for "+chr_name+" for the enzyme "+str(enzyme))
+        print(len(map_restriction))
 
     handle.close();
 
-print "Restriction maps of all chromosomes are now in memory!";
+print("Restriction maps of all chromosomes are now in memory!")
 
 #  Function to assign the corresponding restriction fragment to a locus.
 def find_frag(chrm,locus) :
@@ -89,7 +89,7 @@ with open(sys.argv[3]) as f: # open the file for reading
     for line in f: # iterate over each line
         i=i+1;
         if i % 1000000 == 0:
-            print str(i)+" lines have been attributed restriction fragments.";
+            print(str(i)+" lines have been attributed restriction fragments.";
         chr1, locus1, sens1, chr2, locus2, sens2 = line.split(); # split it by whitespace
         locus1=int(locus1);sens1=int(sens1);
         locus2=int(locus2);sens2=int(sens2);        
@@ -98,5 +98,5 @@ with open(sys.argv[3]) as f: # open the file for reading
         fout.write(str(chr1)+"\t"+str(locus1)+"\t"+str(sens1)+"\t"+str(indice1)+"\t"+str(chr2)+"\t"+str(locus2)+"\t"+str(sens2)+"\t"+str(indice2)+"\n"); 
 
 fout.close();
-print "Output file done!"; 
+print("Output file done!")
 
