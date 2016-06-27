d7eb9f1f67e35dfedd99173c657bbfe455a528ac
axelcournac <acournac@pasteur.fr>
2016-06-27 18:58:47 +0200

    Update lines_jupyter.py

--- python_codes/lines_jupyter.py
+++ python_codes/lines_jupyter.py
@@ -1,1 +1,13 @@
+# lines used to use Jupyter
 
+import numpy as np
+import os
+import matplotlib
+%matplotlib inline
+import mpld3
+mpld3.enable_notebook()
+import pylab
+from pylab import *
+pylab.rcParams['figure.figsize'] = (10, 10)
+m=loadtxt("/data/tpdata/hic/MAT_RAW.txt")
+imshow(m**0.2,interpolation="none")
