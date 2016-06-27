a3d9a3b681b91900e54b037297031c57b1bc3a9e
axelcournac <acournac@pasteur.fr>
2016-06-27 18:42:14 +0200

    Update python_codestlines_jupyler.py

--- python_codestlines_jupyler.py
+++ python_codestlines_jupyler.py
@@ -6,7 +6,8 @@ import matplotlib
 %matplotlib inline
 import mpld3
 mpld3.enable_notebook()
-
+import pylab
+from pylab import *
 pylab.rcParams['figure.figsize'] = (10, 10)
 m=loadtxt("/data/tpdata/hic/MAT_RAW.txt")
 imshow(m**0.2,interpolation="none")
